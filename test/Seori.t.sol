e7ef246b324331c13a14f2d3158053ca5049b22a
edy <edy.sassolino@gmail.com>
2023-05-14 01:18:53 +0900

    add airdropMint revert test

--- test/Seori.t.sol
+++ test/Seori.t.sol
@@ -20,4 +20,25 @@ contract CounterTest is Test {
     function testTokenURI() public {
         assertEq(nft.tokenURI(1), string.concat(BASE_URI_FOR_TEST, vm.toString(uint256(1)), ".json"));
     }
+
+    function test_expectRevert_airdropMint_amountZero() public {
+        nft.grantRole(nft.AIRDROP_ROLE(), address(this));
+        vm.expectRevert();
+        address[] memory p1 = new address[] (1);
+        uint256[] memory p2 = new uint256[] (1);
+        p1[0] = vm.addr(1);
+        nft.airdropMint(p1, p2);
+
+    }
+
+    function test_expectRevert_airdropMint_exceedSupply() public {
+        nft.grantRole(nft.AIRDROP_ROLE(), address(this));
+        address[] memory p1 = new address[] (1);
+        uint256[] memory p2 = new uint256[] (1);
+        p1[0] = vm.addr(1);
+        p2[0] = nft.maxSupply();
+        vm.expectRevert();
+        nft.airdropMint(p1, p2);
+
+    }
 }
